0a0128e92e6f63280c591a3407f844c224f755f8
David <dblancou@alumnos.unex.es>
2023-12-04 09:58:01 +0100

    He modificado la DemoTwoPassengers ya que salian algunos errores y eran porque estaban maal llamados los comparadores y el Actor era el Taxi

--- DemoTwoPassengers.java
+++ DemoTwoPassengers.java
@@ -9,10 +9,10 @@ import java.util.*;
  * @version 2016.02.29
  * @version 2023 DP Classes
  */
-public class DemoTwoPassengers
+public  class DemoTwoPassengers
 {
      TransportCompany company;
-    private List<Actor> actors;
+    private List<Taxi> actors;
 
     /**
      * Constructor for objects of class DemoOnePassanger
@@ -43,7 +43,7 @@ public class DemoTwoPassengers
      */
     public void step()
     {
-        for(Actor actor : actors) {
+        for(Taxi actor : actors) {
             actor.act();
         }
     }
@@ -117,7 +117,7 @@ public class DemoTwoPassengers
             System.out.println(taxi);
         }
         System.out.println("-->> Passengers requesting taxi <<--");
-        Collections.sort(passengers, new ComparadorPasHoraLlegada());
+        Collections.sort(passengers, new ComparadorArrivalTime());
 
         for(Passenger passenger : passengers) {
             System.out.println(passenger);
@@ -135,7 +135,7 @@ public class DemoTwoPassengers
     private void showFinalInfo() {
         List<Taxi> vehicles = company.getVehicles();
         List<Passenger> passengers = company.getPassengers();
-        Collections.sort(vehicles, new ComparadorPasTransportadosTaxi());
+        Collections.sort(vehicles, new ComparadorNumPassengers());
         Collections.sort(passengers, new ComparadorNombrePassenger());
 
         System.out.println("");
